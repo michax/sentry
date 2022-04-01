697957013c6cb2e1d29dcda84c7c6ed81bda1288
maggie bauer <maggie@sentry.io>
2022-04-01 15:04:53 -0700

    Add audit log manager tests

--- tests/sentry/models/test_auditlogentry.py
+++ tests/sentry/models/test_auditlogentry.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 
-from sentry.models import AuditLogEntry, AuditLogEntryEvent
+from sentry.models import AuditLogEntry, AuditLogEntryEvent, AuditLogEvent, audit_log_manager
 from sentry.testutils import TestCase
 
 
@@ -32,3 +32,20 @@ class AuditLogEntryTest(TestCase):
             entry.get_note()
             == "changed plan to Team with 50K errors, 100K transactions, and 1 GB of attachments"
         )
+
+
+class AuditLogEventManagerTest(TestCase):
+    def test_audit_log_manager(self):
+        self.data = {"email": "my_email@gmail.com"}
+        log_event = AuditLogEvent(
+            event_id=1,
+            name="member_invite",
+            api_name="member.invite",
+            get_note="invited member {}".format(self.data["email"]),
+        )
+
+        audit_log_manager.add(log_event)
+
+        assert audit_log_manager.get(event_id=1) == log_event
+        assert audit_log_manager.get_event_id(name="member_invite") == 1
+        assert audit_log_manager.get_api_names() == ["member.invite"]
